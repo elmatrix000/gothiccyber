c32c6b053e92377215815f5185999a82f47e9951
elmatrix000 <internetflytrap@proton.me>
2026-02-24 15:49:16 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function scrollSlider(button, direction) {
-    const slider = button.parentElement.querySelector(".slider");
-    const scrollAmount = slider.clientWidth * 0.8;
-    slider.scrollBy({
-        left: direction * scrollAmount,
-        behavior: "smooth"
-    });
-}
