555d11d092c627d3763686ecdc53e46af76cca42
elmatrix000 <internetflytrap@proton.me>
2026-02-24 16:11:00 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function openModal(img){
+document.getElementById("modal").style.display="flex";
+document.getElementById("modalImg").src=img.src;
+}
+
+function closeModal(){
+document.getElementById("modal").style.display="none";
+}
